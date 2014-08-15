25f6c7813696fc23878a468fbbddc564d72f2b1a
dan stone <dan.stone16321@gmail.com>
2014-08-15 09:00:05 +0100

    go-slow boot fn for my poor laptop (power saving)

--- src/ironcast/boot.clj
+++ src/ironcast/boot.clj
@@ -1,4 +1,16 @@
 (ns ironcast.boot
-  (:require [ironcast.core :refer :all]))
+  (:require [ironcast.core :refer :all]
+            [ironcast.api :as api]))
 
+
+(defn go-slow
+  []
+  (api/set-setting :max-fps 15))
+
+(defn go-fast
+  []
+  (api/set-setting :max-fps 0))
+
+
+(go-slow)
 (go)
